7c2dd875b3cb4f9f837ed0f30b104cbbaff35562
Jacksonchong11 <chong_qi_jun@soc.uum.edu.my>
2023-04-26 12:21:19 +0800

    Added RIve

--- brand_qr_scanner/lib/views/loginscreen.dart
+++ brand_qr_scanner/lib/views/loginscreen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-
 import '../../models/user.dart';
 import '../constants.dart';
 
